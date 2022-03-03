90be38fbf5c2879381b6bd313c9845ce2e29d79e
Vladimir Vovk <vovkv231@gmail.com>
2022-03-03 20:42:01 +0500

    Initial commit

---
+++ src/main/java/hello/service/impl/GreetingServiceImpl.java
@@ -0,0 +1,9 @@
+package hello.service.impl;
+
+import hello.service.GreetingService;
+
+public class GreetingServiceImpl implements GreetingService {
+    public void sayHello(){
+        System.out.println("Hello OSGi World!");
+    }
+}
