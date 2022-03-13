4a11f1475c86b1f37a3a44ace7642079c3142739
vovkv <vovkv231@gmail.com>
2022-03-14 00:58:17 +0500

    Using @GogoCommand

--- Stage4/src/main/java/ru/ifmo/vovk/command/HelloCommand.java
+++ Stage4/src/main/java/ru/ifmo/vovk/command/HelloCommand.java
@@ -1,18 +1,14 @@
 package ru.ifmo.vovk.command;
 
+
+import org.apache.felix.service.command.annotations.GogoCommand;
 import org.osgi.service.component.annotations.Component;
 
-@Component(
-        property = {
-                "osgi.command.scope=practice",
-                "osgi.command.function=hello"
-        },
-        service=HelloCommand.class
-)
+@GogoCommand(scope = "practice", function = "hello")
+@Component(service=Object.class)
 public class HelloCommand {
 
     public String hello(String name) {
-        return "Hello, " + name;
+        return "Hello " + name;
     }
-
 }
